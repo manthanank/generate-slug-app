2202506d5bd94323a9268c502930e17a214dbb02
Manthan Ankolekar <manthan.ank46@gmail.com>
2023-07-27 18:46:31 +0530

    initial code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'generate-slug-app';
+  title: string = '';
+  slug: string = '';
+  showCopyMessage: boolean = false;
+
+  generateSlug() {
+    this.slug = this.title
+      .toLowerCase()
+      .replace(/[^a-z0-9]+/g, '-')
+      .replace(/(^-|-$)/g, '');
+  }
+
+  copySlug() {
+    const textarea = document.createElement('textarea');
+    textarea.value = this.slug;
+    document.body.appendChild(textarea);
+    textarea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textarea);
+
+    this.showCopyMessage = true;
+    setTimeout(() => {
+      this.showCopyMessage = false;
+    }, 2000); // Hide the message after 2 seconds
+  }
 }
