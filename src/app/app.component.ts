094f259f2f207e7d3c2277a9ee1208cc23238683
Manthan Ankolekar <manthan.ank46@gmail.com>
2024-06-01 10:29:20 +0530

    chore: Update Angular dependencies to version 18.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
